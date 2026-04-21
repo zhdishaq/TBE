d29dde6f6698f90750c10cf6f44fb700c8eb2699
zhdishaq <zhdishaq@yahoo.com>
2026-04-21 03:22:06 +0300

    up

---
+++ src/services/BookingService/BookingService.API/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TBE.BookingService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> all()
+        {
+            return Ok(new { message="testing"});
+        }
+
+
+    }
+}
